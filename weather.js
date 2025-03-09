194727ac7924c2ac84bc6401621ad52caeaedffc
Abdul Baari <164978787+Abdul-Baari-2003@users.noreply.github.com>
2025-03-09 10:47:52 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const API_KEY = "";
+const API_KEY = "YOUR_API_KEY";
 
 async function getWeather(city) {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
